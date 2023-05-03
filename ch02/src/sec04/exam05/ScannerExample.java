375fccbb7423ca66871878820478a77e9a64ed2b
Your Name <sr.hong0114@gmail.com>
2023-05-03 18:19:52 +0900

    fourth commit

---
+++ ch02/src/sec04/exam05/ScannerExample.java
@@ -0,0 +1,20 @@
+package sec04.exam05;
+
+import java.util.Scanner;
+
+public class ScannerExample {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		String inputData;
+		
+		while(true) {
+			inputData = scanner.nextLine();
+			System.out.println(inputData);
+			if(inputData.contentEquals("q")) {
+				break;
+			}
+		}
+		
+		System.out.println("종료");
+	}
+}
