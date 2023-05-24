0c5fb4ac6fb3e123a6bede99d81b99dc4019bdca
magrita00 <sr.hong0114@gmail.com>
2023-05-24 17:57:43 +0900

    first commit

---
+++ test/src/sec01/exam01/StudentExample.java
@@ -0,0 +1,13 @@
+package sec01.exam01;
+
+public class StudentExample {
+
+	public static void main(String[] args) {
+		Student s1 = new Student();
+		System.out.println("끝");
+		
+		Student s2 = new Student();
+		System.out.println("끝끝");
+	}
+
+}
